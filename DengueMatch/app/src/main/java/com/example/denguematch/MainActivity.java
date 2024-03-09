4f55573e7821e42dd29a30e9415afb6292502ec2
Leonardo Lima <104847717+sirbickle@users.noreply.github.com>
2024-03-09 13:48:44 -0300

    fabrica de projeto

---
+++ DengueMatch/app/src/main/java/com/example/denguematch/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.denguematch;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.loadUrl("https://upx5-omega.vercel.app/index.html");
+
+        }
+
+
+}
